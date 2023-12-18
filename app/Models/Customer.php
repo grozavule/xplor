b6b363b905119d926c752e85cb3af723d13138f3
Eric Drake <puturosule@gmail.com>
2023-12-18 06:32:48 -0700

    fixed a bug affecting attribute order when customers are added to the list

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -10,4 +10,20 @@ class Customer extends Model
     use HasFactory;
 
     public $timestamps = false;
+
+    public function attributesToArray(): array
+    {
+        $attributes = parent::attributesToArray();
+        $attributeOrder = ['id', 'full_name', 'email_address', 'phone_number', 'address'];
+
+        $rearranged = [];
+        foreach($attributeOrder as $attribute)
+        {
+            if(array_key_exists($attribute, $attributes))
+            {
+                $rearranged[$attribute] = $attributes[$attribute];
+            }
+        }
+        return $rearranged;
+    }
 }
